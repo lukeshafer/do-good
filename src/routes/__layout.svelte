655272e59466c0721ef28e4591ab35885ad1b53c
Luke Shafer <hello@lukeshafer.com>
2022-06-01 22:31:25 -0400

    Finished layout for initial, basic site

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Nav from '$lib/components/Nav.svelte';
+  import 'normalize.css';
+  import '../global.css';
+</script>
+
+<Nav>
+  <!-- hold -->
+</Nav>
+
+<slot><!-- optional fallback --></slot>
+
+<style>
+</style>
