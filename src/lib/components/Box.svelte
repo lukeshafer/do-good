7edb137511e5bc777510d4f7b2d1202b74b97fe0
Luke Shafer <hello@lukeshafer.com>
2022-06-03 18:45:47 -0400

    feat: :art: Added Box component
    Added the Box component for the Home Page (though they can and should be used everywhere). Added new colors and images for the box

---
+++ src/lib/components/Box.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let background: string;
+</script>
+
+<article style:background-color={background}>
+  <slot />
+</article>
+
+<style>
+  article {
+    position: relative;
+    padding: 1em;
+    text-align: center;
+    border-radius: 1.5em;
+    border: 2px solid var(--border-color);
+    outline: 0.1px solid rgba(var(--background-values), 0.5);
+  }
+
+  /* Box Shadow -- used ::after instead of box-shadow because of the shadow's border */
+  article::after {
+    position: absolute;
+    padding: inherit;
+    text-align: center;
+    border-radius: inherit;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    content: '';
+    transform: translate(0.5rem, 0.4rem);
+    background: var(--box-shadow-color);
+    border: 2px solid var(--border-color);
+    z-index: -1;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,30 @@
+<script lang="ts">
+  import Box from '$lib/components/Box.svelte';
+</script>
+
 <svelte:head>
   <title>Do Good Collective</title>
 </svelte:head>
 
 <main>
-  <header>
-    <h1>Do Good Collective</h1>
-  </header>
+  <Box background="var(--accent-color)">
+    <h2>Rally for Mr. Talley</h2>
+    <p class="box-amount">Goal: $5,000</p>
+    <img
+      class="box-icon"
+      src="/images/hands-with-heart.svg"
+      alt="Two hands with a cartoon heart between them" />
+    <p class="box-body">
+      Mr. Talley needs a kidney and a pancreas. We're trying to make sure he
+      doesn't have to worry about providing for his kids this summer. Please
+      help us out!
+    </p>
+  </Box>
 </main>
+
+<style>
+  main {
+    display: grid;
+    justify-items: center;
+  }
+</style>
