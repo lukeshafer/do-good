145a229bd658ec04ce05f18bef626b30b892b61f
Luke Shafer <hello@lukeshafer.com>
2022-06-03 17:17:20 -0400

    Rearrange Nav component file structure

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import Header from '$lib/components/Header.svelte';
   import Logo from '$lib/components/Logo.svelte';
-  import Navbar from '$lib/components/Navbar.svelte';
-  import NavItem from '$lib/components/NavItem.svelte';
+  import Navbar from '$lib/components/Nav/Navbar.svelte';
+  import NavItem from '$lib/components/Nav/NavItem.svelte';
   import 'normalize.css';
   import '../global.css';
 </script>
