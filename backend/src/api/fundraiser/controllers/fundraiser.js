0f1eace0b9ef22ae21e8faec7729d24e3516537b
Luke Shafer <lukealanshafer@gmail.com>
2022-06-12 06:35:32 +0000

    feat: :sparkles: Added dynamic fundraiser page which pulls from backend
    Added [slug].svelte under fundraisers, which pulls fundraisers from the backend API.
    
    Make sure to npm update after this one!

--- backend/src/api/fundraiser/controllers/fundraiser.js
+++ backend/src/api/fundraiser/controllers/fundraiser.js
@@ -6,4 +6,21 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::fundraiser.fundraiser');
+module.exports = createCoreController(
+  'api::fundraiser.fundraiser',
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { slug } = ctx.params;
+
+      const entity = await strapi.db
+        .query('api::fundraiser.fundraiser')
+        .findOne({
+          where: { slug },
+        });
+
+      const sanitizedEntity = await this.sanitizeOutput(entity);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
