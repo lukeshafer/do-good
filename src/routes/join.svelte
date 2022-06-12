2770e8179f3cf8f85272a1840bb1d1339fc153cc
K Waters <waterskm2013@gmail.com>
2022-06-12 13:30:35 -0400

    embedded google form on join

--- src/routes/join.svelte
+++ src/routes/join.svelte
@@ -3,11 +3,22 @@
 </script>
 
 <main>
-  <!--main-->
+  <iframe
+    title="Join Do Good Collective"
+    src="https://docs.google.com/forms/d/e/1FAIpQLScmrJZjril0W95IoL-hvDIBKTzE_ZECMmdI4gAnBb409ARxEg/viewform?embedded=true"
+    width="100%"
+    height="2000"
+    frameborder="0"
+    marginheight="0"
+    marginwidth="0">Loading…</iframe>
 </main>
 
 <style>
   main {
     display: flex;
+  }
+  iframe {
+    display: flex;
+    justify-content: flex-start;
   }
 </style>
