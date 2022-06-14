8461ec58e21669ad5d707bb48ed85e265f00d24f
Luke Shafer <lukealanshafer@gmail.com>
2022-06-14 02:17:56 +0000

    feat(Backend): :art: Added Navigation Menu Type
    Added single content type called Navigation Menu for determining which pages are on the NavBar

---
+++ backend/src/api/navigation-menu/services/navigation-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * navigation-menu service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::navigation-menu.navigation-menu');
